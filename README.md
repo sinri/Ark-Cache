975ca8182a124282ee97f27c11ecbe5610e23795
Sinri Edogawa <e.joshua.s.e@gmail.com>
2018-09-07 10:53:31 +0800

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ark-Cache
 The cache component for Ark 2
+
+Provided Dummy and File implementation.
---
+++ src/ArkCache.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sinri
+ * Date: 2018/9/7
+ * Time: 10:39
+ */
+
+namespace sinri\ark\cache;
+
+
+interface ArkCache
+{
+    /**
+     * @param string $key
+     * @param mixed $object
+     * @param int $life 0 for no limit, or seconds
+     * @return bool
+     */
+    public function saveObject($key, $object, $life = 0);
+
+    /**
+     * @param string $key
+     * @return mixed|bool
+     */
+    public function getObject($key);
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function removeObject($key);
+
+    /**
+     * @return bool
+     */
+    public function removeExpiredObjects();
+}
